58fb77b2d0f10277d714ad943c2348dca3084257
Viktor Piven <viktor.piven@eleks.com>
2022-08-22 13:41:19 +0300

    Fix language

--- src/main/java/com/viktor/calculator/utils/Menu.java
+++ src/main/java/com/viktor/calculator/utils/Menu.java
@@ -6,12 +6,12 @@ public class Menu {
 
     public int menuChooseMode() {
         System.out.println("Main menu\n" +
-                "1. Посчитать выражение\n" +
-                "2. Получить все выражения\n" +
-                "3. Найти выражения значения которых равно числу\n" +
-                "4. Найти выражения значения которых больше\n" +
-                "5. Найти выражения значения которых меньше\n" +
-                "6. Обновить выражение\n" +
+                "1. Calculate expression\n" +
+                "2. Find all expressions in database\n" +
+                "3. Find expressions where result is equals to a number\n" +
+                "4. Find expressions where result is greater than\n" +
+                "5. Find expressions where result is less than\n" +
+                "6. Update expression\n" +
                 "0. Exit");
         System.out.println();
         return new Scanner(System.in).nextInt();
