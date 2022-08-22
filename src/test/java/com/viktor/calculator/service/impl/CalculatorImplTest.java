0336578d2440e53a84404d2c856802aa34d32544
Viktor Piven <viktor.piven@eleks.com>
2022-08-22 17:50:30 +0300

    Fix test

--- src/test/java/com/viktor/calculator/service/impl/CalculatorImplTest.java
+++ src/test/java/com/viktor/calculator/service/impl/CalculatorImplTest.java
@@ -1,6 +1,5 @@
 package com.viktor.calculator.service.impl;
 
-import com.viktor.calculator.exceptions.OperationException;
 import com.viktor.calculator.service.CalculateService;
 import org.junit.jupiter.api.Test;
 
@@ -17,36 +16,36 @@ class CalculatorImplTest {
 
     @Test
     void calculatePlusMinusOk() {
-        String expression = "5 + 1 - 5 + 12 + 12.7";
+        String expression = "5+1-5+12+12.7";
         double result = calculateService.calculate(expression);
         assertEquals(25.7, result);
     }
 
     @Test
     void calculateMultiplyDivideOk() {
-        String expression = "5 * 1 - 12 / 2 + 12.7";
+        String expression = "5*1-12/2+12.7";
         double result = calculateService.calculate(expression);
         assertEquals(11.7, result);
     }
 
     @Test
     void calculateWithBracketsOk() {
-        String expression = "5 * ( 1 - 12 ) / 2 + ( 12.7 + 5 )";
+        String expression = "5*(1-12)/2+(12.7+5)";
         double result = calculateService.calculate(expression);
         assertEquals(-9.8, result);
     }
 
     @Test
     void calculateWithNegativeNumberOk() {
-        String expression = "5 * -5";
+        String expression = "5*-5";
         double result = calculateService.calculate(expression);
         assertEquals(-25, result);
     }
 
     @Test
     void failIf2OperationInRow() {
-        String expression = "5 * + 5";
-        assertThrows(OperationException.class, () -> calculateService.calculate(expression));
+        String expression = "5+*5";
+        assertThrows(NumberFormatException.class, () -> calculateService.calculate(expression));
     }
 
 }
